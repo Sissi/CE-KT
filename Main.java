c7fc5bca1dc16bc42f9350051695fc288313552e
Simone Obereigner <luna6@gmx.at>
2011-11-29 12:02:06 +0100

    <Tag 11> Artur: ändert das Anlegen der Listenelemente in der Main-Klasse

--- Main.java
+++ Main.java
@@ -9,11 +9,11 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		Set<DomainObject> objectSet = new HashSet<DomainObject>();
-		objectSet.add(new DomainObject("Helga", "Kommentar1"));
-		objectSet.add(new DomainObject("Walter", "Kommentar2"));
-		objectSet.add(new DomainObject("Hans", "Kommentar3"));
-		objectSet.add(new DomainObject("Paul", "Kommentar4"));
-		objectSet.add(new DomainObject("Claudia", "Kommentar5"));
+		objectSet.add(DomainFactory.createDomainObject("Helga", "Kommentar1"));
+		objectSet.add(DomainFactory.createDomainObject("Walter", "Kommentar2"));
+		objectSet.add(DomainFactory.createDomainObject("Hans", "Kommentar3"));
+		objectSet.add(DomainFactory.createDomainObject("Paul", "Kommentar4"));
+		objectSet.add(DomainFactory.createDomainObject("Claudia", "Kommentar5"));
 		for (DomainObject domainObject : objectSet) {
 			LOGGER.debug(domainObject.getUuid);
 		}
