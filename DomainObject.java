2e7b2bb8debb97a47f43a2b330a261f865fcfffe
Simone Obereigner <luna6@gmx.at>
2011-11-29 10:37:22 +0100

    <Tag 4> Artur: String comment samt getter- und setter-Methoden hinzugefügt

--- DomainObject.java
+++ DomainObject.java
@@ -4,6 +4,7 @@ public class DomainObject {
 
 	private String uuid;
 	private String name;
+	private String comment;
 
   /** 
    *  Default constructor
@@ -22,5 +23,13 @@ public class DomainObject {
 	}
 	
 	public String id = UUID.random().toString()
+	
+	public String getComment(){
+		return comment;
+	}
+	
+	public String setComment(String comment){
+		this.comment = comment;
+	}
 
 }
